f54ea233bb32539178289f4211f464e8b82e7334
diegoshhh <diegoshh@gmail.com>
2022-08-03 04:18:47 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {NavigationContainer} from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { TailwindProvider } from 'tailwindcss-react-native'
+import HomeScreen from './screens/HomeScreen'
+
+
+
+const Stack = createNativeStackNavigator()
+
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <TailwindProvider>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen}/>
+        </Stack.Navigator>
+      </TailwindProvider>
+    </NavigationContainer>
+  )
+}
+
+export default App
